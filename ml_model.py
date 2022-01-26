a48687367f757b1e5566c72c173fd68d01dac994
AjayDabral <ajaydabral007@gmail.com>
2022-01-27 03:14:25 +0530

    3 models of ml added

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ML model related code to the software")
+
+def model1:
+    print("this is model1")
+
+def model2:
+    print("this is model2")
+
+def model3:
+    print("this is model3")
