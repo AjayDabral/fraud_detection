985158652a65b6a92687ac0007f543cec1d86be9
AjayDabral <ajaydabral007@gmail.com>
2022-01-27 02:14:53 +0530

    arithematic functions added

---
+++ functions.py
@@ -0,0 +1,15 @@
+print("this is the code for sum")
+
+a=6
+b=7
+def sum(a,b):
+    c=a+b
+    return(c)
+
+def multiplication(a,b):
+    d=a*b
+    return(d)
+
+def division(a,b):
+    e=a/b
+    return(e)
